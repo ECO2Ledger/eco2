ed6b6123305f7712932602c9fd2c54bcd2b18b37
cryptomancc <cryptoman@eco2.cc>
2020-12-16 18:43:08 +0800

    script: support extra eco2 arguments

--- scripts/start_local_testnet.sh
+++ scripts/start_local_testnet.sh
@@ -5,13 +5,15 @@ PROG_DIR=$(readlink -m $(dirname $0))
 ARGS="$@"
 ARGC=$#
 
-if [ $ARGC -ne 2 ]; then
+if [ $ARGC -lt 2 ]; then
   echo "Usage: $PROG_NAME <Number> <Base Path>"
   exit 1
 fi
 
 NODE_NUM=$1
-BASE_PATH=$2
+shift
+BASE_PATH=$1
+shift
 
 mkdir -p $BASE_PATH
 
@@ -19,13 +21,16 @@ PORT=`expr 30333 + $NODE_NUM`
 WS_PORT=`expr 9944 + $NODE_NUM`
 RPC_PORT=`expr 9933 + $NODE_NUM`
 
-./target/release/eco2 \
+cmd="./target/release/eco2 \
   --base-path $BASE_PATH \
   --chain ./config/local-testnet.json \
   --port $PORT \
   --ws-port $WS_PORT \
   --rpc-port $RPC_PORT \
-  --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
   --validator \
   --rpc-methods Unsafe \
-  --name "Node$NODE_NUM"
+  --name 'Node$NODE_NUM'
+  $@"
+  
+echo $cmd
+nohup $cmd > $BASE_PATH/debug.log &
